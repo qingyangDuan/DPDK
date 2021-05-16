e93c58f2942c7ec14053c6a2579e9caf2c0df807
duanqingyang <33786097+duanqingyang@users.noreply.github.com>
2021-05-17 00:34:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
+(1) install ofed:
+
+download mlx-ofed-4.9
+tar -xf ..  && cd 
 ./mlnxofedinstall --dpdk --upstream-libs
 
+sudo vim /etc/modprobe.d/mlx4_core.conf
+add: options mlx4_core log_num_mgm_entry_size=-1
+sudo service openibd restart
 
+(2) install dpdk:
+
+download dpdk-18 or 20.11.1
+pip3 install --user meson
+meson <options> build 
+cd build
+ninja
+ninja install
+ldconfig
